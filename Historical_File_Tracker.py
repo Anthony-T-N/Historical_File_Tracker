f5345cd0ff06e8ba3ad7e73336ceb10f6a97c70e
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2023-06-14 21:04:01 +1000

    Update Historical_File_Tracker.py

--- Historical_File_Tracker.py
+++ Historical_File_Tracker.py
@@ -10,11 +10,22 @@ def read_files():
   return dir_list
 
 def write_to_text(list):
+  input_file = open("p[]-old.txt", "r")
+  input_file_lines = input_file.readlines()
   output_file = open("p[].txt", "w")
+
+  count = 0
+  for current_line in input_file_lines:
+    count += 1
+    print("Line{}: {}".format(count, current_line.strip()))
+
   for item in list:
     print(item)
     output_file.write(item + "\n")
+  input_file.close()
   output_file.close()
+  os.remove("p[]-old.txt")
+  os.rename('p[].txt', 'p[]-old.txt')
 
 def main():
   print("Running")

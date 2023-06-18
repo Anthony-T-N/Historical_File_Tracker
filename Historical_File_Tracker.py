62622892aba0b4ce2186a3f3ba8884585f441412
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2023-06-18 12:11:22 +1000

    Update Historical_File_Tracker.py

--- Historical_File_Tracker.py
+++ Historical_File_Tracker.py
@@ -4,62 +4,69 @@ __date__ = '2023-06-12'
 
 import os
 
-def write_to_text(directory_list, historical_file, temp_file):
-  input_file = open(historical_file, "r")
-  input_file_lines = input_file.readlines()
-  output_file = open(temp_file, "w")
-  deleted_list_file = open("deleted_files", "w")
+def write_to_text(directory_list, historical_file, temp_write_file):
+  historical_file = open(historical_file, "r")
+  historical_file_lines = historical_file.readlines()
+  temp_write_file = open(temp_write_file, "w")
+  deleted_list_file = open("deleted_files", "a")
 
   range_num = 0
-  print(str(len(input_file_lines)) + "+" + str(len(directory_list)))
+  print(str(len(historical_file_lines)) + "+" + str(len(directory_list)))
 
   # Usually new file.
-  if (len(input_file_lines) == 0):
+  if (len(historical_file_lines) == 0):
     for i in range(len(directory_list)):
-      output_file.write(directory_list[i] + "\n")
-    input_file.close()
-    output_file.close()
+      temp_write_file.write(directory_list[i] + "\n")
+    historical_file.close()
+    temp_write_file.close()
     os.remove(historical_file)
-    os.rename(temp_file, historical_file)
+    os.rename(temp_write_file, historical_file)
     return
 
-  if (len(input_file_lines) >= len(directory_list)):
-    range_num = len(input_file_lines)
+  if (len(historical_file_lines) >= len(directory_list)):
+    range_num = len(historical_file_lines)
   else:
     range_num = len(directory_list)
 
   print(range_num)
 
-  input_line_count = 0
+  historical_file_count = 0
   directory_list_count = 0
   for i in range(range_num):
-    print(input_file_lines[input_line_count].strip() + " | " + directory_list[directory_list_count].strip())
-    if (input_file_lines[input_line_count].strip() != directory_list[directory_list_count].strip()):
+    
+    if (historical_file_count >= range_num or directory_list_count >= range_num):
+      break
+
+    print("DEBUG SECTION: historical_file_count: " + str(historical_file_count) + " directory_list_count: " + str(directory_list_count) + "\n")
+
+    print(historical_file_lines[historical_file_count].strip() + " | " + directory_list[directory_list_count].strip())
+    if (historical_file_lines[historical_file_count].strip() != directory_list[directory_list_count].strip()):
       print("UNMATCH")
-      deleted_list_file.write(input_file_lines[input_line_count].strip() + "\n")
+      deleted_list_file.write(historical_file_lines[historical_file_count].strip() + "\n")
+      temp_write_file.write(directory_list[directory_list_count].strip() + "\n")
       directory_list_count += 1
     else:
-      output_file.write(directory_list[directory_list_count] + "\n")
-      input_line_count += 1
+      temp_write_file.write(directory_list[directory_list_count].strip() + "\n")
+      historical_file_count += 1
       directory_list_count += 1
 
   print("!@")
 
-  input_file.close()
-  output_file.close()
+  historical_file.close()
+  temp_write_file.close()
   os.remove(historical_file)
-  os.rename(temp_file, historical_file)
+  os.rename(temp_write_file, historical_file)
 
 # Recursively read all files in directory and store as list.
 def read_directory():
-  dir_list = os.listdir(".\\Sample_Directory")
+  dir_list = os.listdir(".\\Sample_Directory\\")
   return dir_list
 
 def main():
   print("Running")
   historical_file = ".\\p[]-old.txt"
-  temp_file = ".\\p[].txt"
-  write_to_text(read_directory(), historical_file, temp_file)
+  temp_write_file = ".\\p[].txt"
+  write_to_text(read_directory(), historical_file, temp_write_file)
 
 if __name__ == '__main__':
   main()
